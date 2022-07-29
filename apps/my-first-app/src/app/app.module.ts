94e4082e43f65f152556a73778489dafcfbfd850
Timo Nohe <timo.ademeit@gmx.de>
2022-07-29 13:07:57 +0200

    16. How an Angular App gets Loaded and Started

--- apps/my-first-app/src/app/app.module.ts
+++ apps/my-first-app/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule
   ],
