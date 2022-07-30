8cf8104eb6c238805209248bfc08ddb94fd0674b
Timo Nohe <timo.ademeit@gmx.de>
2022-07-30 16:36:20 +0200

    21. Creating Components with the CLI & Nesting Components

--- apps/my-first-app/src/app/app.module.ts
+++ apps/my-first-app/src/app/app.module.ts
@@ -3,15 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-  ],
-  imports: [
-    BrowserModule,
-  ],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
