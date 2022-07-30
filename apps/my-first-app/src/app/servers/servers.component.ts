49b78326ce65b562e179d7d5e10a43e99eb133fe
Timo Nohe <timo.ademeit@gmx.de>
2022-07-30 16:39:35 +0200

    22. Working with Component Templates

--- apps/my-first-app/src/app/servers/servers.component.ts
+++ apps/my-first-app/src/app/servers/servers.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+  `,
   styleUrls: ['./servers.component.scss'],
 })
 export class ServersComponent {
