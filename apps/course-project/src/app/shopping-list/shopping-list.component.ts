1ceeb96f61d4e60988e85e30b47c2f7bcf5c1b28
Timo Nohe <timo.ademeit@gmx.de>
2022-08-03 12:11:33 +0200

    58. Creating an "Ingredient" Model

--- apps/course-project/src/app/shopping-list/shopping-list.component.ts
+++ apps/course-project/src/app/shopping-list/shopping-list.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: [];
+
   constructor() {}
 
   ngOnInit(): void {}
