a36da5183b28862589aac189c1c204c477231a7a
Timo Nohe <timo.ademeit@gmx.de>
2022-08-18 12:43:16 +0200

    79. Seeing Lifecycle Hooks in Action

--- apps/components-databinding-deepdive/src/app/server-element/server-element.component.ts
+++ apps/components-databinding-deepdive/src/app/server-element/server-element.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+/* eslint-disable @angular-eslint/no-conflicting-lifecycle */
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'cdd-server-element',
@@ -6,11 +7,44 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.scss'],
   encapsulation: ViewEncapsulation.Emulated // None, Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   // eslint-disable-next-line @angular-eslint/no-input-rename
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
 
-  constructor() {}
+  constructor() {
+    console.log('constructor called');
+  }
 
-  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called!', changes);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called!');
+  }
+
+  ngDoCheck(): void {
+      console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit(): void {
+      console.log('ngAfterContentInit called!');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentCalled called!');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called!');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewCalled called!');
+  }
+
+  ngOnDestroy(): void {
+      console.log('ngOnDestroy called!');
+  }
 }
