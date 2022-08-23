f0c65feb99082fc0b4a3d74960189fb25128690c
Timo Nohe <timo.ademeit@gmx.de>
2022-08-23 14:19:18 +0200

    101. Building a Structural Directive

--- apps/directives-deep-dive/src/app/app.module.ts
+++ apps/directives-deep-dive/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { UnlessDirective } from './unless/unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BetterHighlightDirective,
     BasicHighlightDirective,
+    UnlessDirective,
   ],
   imports: [BrowserModule],
   providers: [],
