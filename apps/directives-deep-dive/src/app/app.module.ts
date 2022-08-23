3c8da579a4cb8754cbe9589d8739203c261c6c18
Timo Nohe <timo.ademeit@gmx.de>
2022-08-23 13:37:02 +0200

    94. Creating a Basic Attribute Directive

--- apps/directives-deep-dive/src/app/app.module.ts
+++ apps/directives-deep-dive/src/app/app.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    BasicHighlightDirective,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/directives-deep-dive/src/app/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, ElementRef, OnInit } from "@angular/core";
+
+@Directive({
+  selector: '[directivesDeepDiveBasicHighlight]'
+})
+export class BasicHighlightDirective implements OnInit {
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit(): void {
+    this.elementRef.nativeElement.style.backgroundColor = 'green';
+  }
+}
