ca6e2970211cb32a7d9e942ac01027746e8ba8b5
Timo Nohe <timo.ademeit@gmx.de>
2022-08-23 12:59:25 +0200

    88. Passing Data with Event and Property Binding (Combined)

--- apps/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ apps/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [];
 
   constructor() {
@@ -17,10 +19,14 @@ export class RecipeListComponent {
         imagePath: 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
       },
       {
-        name: 'A Test Recipe',
+        name: 'Another Test Recipe',
         description: 'This is simply a test',
         imagePath: 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
       }
     ]
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 }
--- apps/course-project/src/app/recipes/recipes.component.ts
+++ apps/course-project/src/app/recipes/recipes.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'course-project-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.scss'],
 })
-export class RecipesComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class RecipesComponent {
+  selectedRecipe: Recipe;
 }
