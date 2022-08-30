ae506e59d518578c2c88d7ea8a22f33fc920803f
Timo Nohe <timo.ademeit@gmx.de>
2022-08-30 13:13:19 +0200

    108. Injecting the Logging Service into Components

--- apps/services-showcase/src/app/account/account.component.ts
+++ apps/services-showcase/src/app/account/account.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'services-showcase-account',
@@ -10,9 +11,10 @@ export class AccountComponent {
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  constructor(private loggingService: LoggingService) {}
 
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.loggingService.logStatusChange(status);
   }
 }
