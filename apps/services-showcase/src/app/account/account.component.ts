85ca71bc99e317a18c833f42336a097a80c9b3e4
Timo Nohe <timo.ademeit@gmx.de>
2022-09-06 12:15:30 +0200

    112. Injecting Services into Services

--- apps/services-showcase/src/app/account/account.component.ts
+++ apps/services-showcase/src/app/account/account.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { AccountService } from '../account.service';
-import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'services-showcase-account',
@@ -11,10 +10,9 @@ export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
 
-  constructor(private loggingService: LoggingService, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
 
   onSetTo(status: string) {
     this.accountService.updateStatus(this.id, status);
-    this.loggingService.logStatusChange(status);
   }
 }
