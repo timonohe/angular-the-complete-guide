3b2f32fb805ea827a10afe2913f12a1fbf380ec8
Timo Nohe <timo.ademeit@gmx.de>
2022-09-06 12:21:33 +0200

    113. Using Services for Cross-Component Communication

--- apps/services-showcase/src/app/account/account.component.ts
+++ apps/services-showcase/src/app/account/account.component.ts
@@ -14,5 +14,6 @@ export class AccountComponent {
 
   onSetTo(status: string) {
     this.accountService.updateStatus(this.id, status);
+    this.accountService.statusUpdated.emit(status);
   }
 }
--- apps/services-showcase/src/app/new-account/new-account.component.ts
+++ apps/services-showcase/src/app/new-account/new-account.component.ts
@@ -9,7 +9,11 @@ import { LoggingService } from '../logging.service';
 })
 export class NewAccountComponent {
 
-  constructor(private loggingService: LoggingService, private accountService: AccountService) {}
+  constructor(private loggingService: LoggingService, private accountService: AccountService) {
+    this.accountService.statusUpdated.subscribe(
+      (status: string) => alert(`New Status: ${status}`)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
