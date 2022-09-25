3d9c0086952a48b6b20c098c89a6a25a90d550ed
Timo Nohe <timo.ademeit@gmx.de>
2022-09-25 11:17:31 +0200

    131. Navigating Programmatically

--- apps/routing-showcase/src/app/home/home.component.ts
+++ apps/routing-showcase/src/app/home/home.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'routing-showcase-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
+export class HomeComponent {
 
-  ngOnInit(): void {}
+  constructor(private router: Router) { }
+
+  onLoadServers() {
+    // complex calculation
+    this.router.navigate(['/servers']);
+  }
 }
