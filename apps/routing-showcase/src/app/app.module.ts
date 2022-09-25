db98d80c9a46d38d029db79ed6d269d86fdd3aee
Timo Nohe <timo.ademeit@gmx.de>
2022-09-25 13:16:22 +0200

    143. Redirecting and Wildcard Routes

--- apps/routing-showcase/src/app/app.module.ts
+++ apps/routing-showcase/src/app/app.module.ts
@@ -10,27 +10,28 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { UsersComponent } from './users/users.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
   },
   {
     path: 'users',
     component: UsersComponent,
-    children: [
-      { path: ':id/:name',  component: UserComponent },
-    ]
+    children: [{ path: ':id/:name', component: UserComponent }],
   },
   {
     path: 'servers',
     component: ServersComponent,
     children: [
       { path: ':id', component: ServerComponent },
-      { path: ':id/edit',  component: EditServerComponent },
-    ]
-  }
+      { path: ':id/edit', component: EditServerComponent },
+    ],
+  },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
@@ -42,12 +43,9 @@ const appRoutes: Routes = [
     UserComponent,
     EditServerComponent,
     ServerComponent,
+    PageNotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes)
-  ],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
