33aba2685be6f2852dc9f5bee5f37e1bbbe9c4f5
Timo Nohe <timo.ademeit@gmx.de>
2022-09-27 13:08:09 +0200

    161. Adding Child Routing Together

--- apps/course-project/src/app/app.module.ts
+++ apps/course-project/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { FormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 @NgModule({
   declarations: [
@@ -25,12 +26,9 @@ import { AppRoutingModule } from './app-routing.module';
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
+    RecipeStartComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [ShoppingListService],
   bootstrap: [AppComponent],
 })
---
+++ apps/course-project/src/app/recipes/recipe-start/recipe-start.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'course-project-recipe-start',
+  templateUrl: './recipe-start.component.html',
+  styleUrls: ['./recipe-start.component.scss'],
+})
+export class RecipeStartComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
