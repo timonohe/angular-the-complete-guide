2822cff6b18e8c37b4e40684166b033ddcdaf474
Timo Nohe <timo.ademeit@gmx.de>
2022-10-04 13:08:46 +0200

    182. Changed the Subscription Name

--- apps/course-project/src/app/shopping-list/shopping-list.component.ts
+++ apps/course-project/src/app/shopping-list/shopping-list.component.ts
@@ -10,18 +10,18 @@ import { ShoppingListService } from './shopping-list.service';
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[] = [];
-  private igChangeSub: Subscription;
+  private subscription: Subscription;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit(): void {
     this.ingredients = this.slService.getIngredients();
-    this.igChangeSub = this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
+    this.subscription = this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
       this.ingredients = ingredients;
     });
   }
 
   ngOnDestroy(): void {
-      this.igChangeSub.unsubscribe();
+      this.subscription.unsubscribe();
   }
 }
