24146e11c8a46496187e6a5bfd9996f0450e424c
Timo Nohe <timo.ademeit@gmx.de>
2022-10-26 15:39:26 +0200

    232. Adding new Ingredient Controls

--- apps/course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ apps/course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -58,6 +58,13 @@ export class RecipeEditComponent implements OnInit {
     });
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(new FormGroup({
+      'name': new FormControl(),
+      'amount': new FormControl()
+    }));
+  }
+
   get ingredientcontrols() {
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
