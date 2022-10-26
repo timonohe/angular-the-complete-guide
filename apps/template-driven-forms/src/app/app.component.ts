f7f6fe21b073fb666a3adb02bb4a8d8fc900be6c
Timo Nohe <timo.ademeit@gmx.de>
2022-10-26 10:23:19 +0200

    201. TD: Resetting Forms

--- apps/template-driven-forms/src/app/app.component.ts
+++ apps/template-driven-forms/src/app/app.component.ts
@@ -47,6 +47,8 @@ export class AppComponent {
     this.user.secretQuestion = this.signupForm.value.secret;
     this.user.answer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 
   // onSubmit(form: NgForm) {
