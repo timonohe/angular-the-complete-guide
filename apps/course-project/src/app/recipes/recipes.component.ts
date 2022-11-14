631452f7b3e7a7d82bcb538180bc528c24814589
Timo Nohe <timo.ademeit@gmx.de>
2022-11-14 12:21:43 +0100

    237. Providing the Recipe Service Correctly

--- apps/course-project/src/app/recipes/recipes.component.ts
+++ apps/course-project/src/app/recipes/recipes.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'course-project-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.scss'],
-  providers: [ RecipeService ]
 })
 export class RecipesComponent {
 }
