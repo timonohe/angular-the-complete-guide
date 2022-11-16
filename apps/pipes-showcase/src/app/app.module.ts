462292057ba49ce7bf339c8691a10900732c0ced
Timo Nohe <timo.ademeit@gmx.de>
2022-11-16 08:43:21 +0100

    245. Creating a Custom Pipe

--- apps/pipes-showcase/src/app/app.module.ts
+++ apps/pipes-showcase/src/app/app.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShortenPipe } from './shorten.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    ShortenPipe
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/pipes-showcase/src/app/shorten.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: 'shorten'
+})
+export class ShortenPipe implements PipeTransform {
+  transform(value: string) {
+    if (value.length > 10) {
+      return value.slice(0, 10) + '...';
+    } else {
+      return value;
+    }
+  }
+}
