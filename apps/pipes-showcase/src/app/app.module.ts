5073c341a7b05ed57c6c729c5a2fa05acc7f6275
Timo Nohe <timo.ademeit@gmx.de>
2022-11-16 09:34:14 +0100

    assignment-8-pipes

--- apps/pipes-showcase/src/app/app.module.ts
+++ apps/pipes-showcase/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FilterPipe } from './filter.pipe';
+import { ReversePipe } from './reverse.pipe';
 import { ShortenPipe } from './shorten.pipe';
+import { SortPipe } from './sort.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShortenPipe,
     FilterPipe,
+    ReversePipe,
+    SortPipe,
   ],
   imports: [
     BrowserModule,
