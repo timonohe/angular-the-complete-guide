34a706e6adb81fac7107c8c1fb925b4ac162d458
Timo Nohe <timo.ademeit@gmx.de>
2022-11-16 15:38:37 +0100

    281. Storing Recipes

--- apps/course-project/src/app/header/header.component.ts
+++ apps/course-project/src/app/header/header.component.ts
@@ -1,8 +1,14 @@
 import { Component } from "@angular/core";
+import { DataStorageService } from "../shared/data-storage.service";
 
 @Component({
   selector: 'course-project-header',
   templateUrl: 'header.component.html'
 })
 export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
 }
