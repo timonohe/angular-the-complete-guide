e31fa25f45fc4c816b624dca77da6c5538f8471e
Timo Nohe <timo.ademeit@gmx.de>
2022-11-16 15:49:13 +0100

    282. Fetching Recipes

--- apps/course-project/src/app/shared/data-storage.service.ts
+++ apps/course-project/src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({providedIn: 'root'})
@@ -9,9 +10,16 @@ export class DataStorageService {
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
     this.http
-    .put(
-      'https://ng-course-recipe-book-e8fd7-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
-      recipes
-    ).subscribe(console.log);
+      .put(
+        'https://ng-course-recipe-book-e8fd7-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        recipes
+      )
+      .subscribe(console.log);
+  }
+
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>('https://ng-course-recipe-book-e8fd7-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+      .subscribe(recipes => this.recipeService.setRecipes(recipes));
   }
 }
