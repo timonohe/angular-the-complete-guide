737d7bb4d9ffee779ce2c15cc9fce4e4f0a2236b
Timo Nohe <timo.ademeit@gmx.de>
2022-11-21 12:37:37 +0100

    284. Resolving Data Before Loading

--- apps/course-project/src/app/shared/data-storage.service.ts
+++ apps/course-project/src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from "rxjs";
+import { map, tap } from "rxjs";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
@@ -19,7 +19,7 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
       .get<Recipe[]>('https://ng-course-recipe-book-e8fd7-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
       .pipe(
         map(recipes => {
@@ -29,11 +29,10 @@ export class DataStorageService {
               ingredients: recipe.ingredients ? recipe.ingredients : []
             };
           })
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes)
         })
-      )
-      .subscribe(recipes => {
-        console.log(recipes);
-        this.recipeService.setRecipes(recipes)
-      });
+      );
   }
 }
