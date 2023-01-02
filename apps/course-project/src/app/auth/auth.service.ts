6dbd93e55c3c329d8d145d38f99a8aa7b60942c8
Timo Nohe <timo.ademeit@gmx.de>
2023-01-02 12:51:30 +0100

    301. Adding the Token to Outgoing Requests

--- apps/course-project/src/app/auth/auth.service.ts
+++ apps/course-project/src/app/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { throwError, Subject } from "rxjs";
+import { throwError, BehaviorSubject } from "rxjs";
 import { catchError, tap } from "rxjs/operators";
 import { User } from "./user.model";
 
@@ -15,7 +15,7 @@ export interface AuthResponseData {
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
-  user = new Subject<User>();
+  user = new BehaviorSubject<User>(null);
 
   constructor(private http: HttpClient) {}
 
--- apps/course-project/src/app/shared/data-storage.service.ts
+++ apps/course-project/src/app/shared/data-storage.service.ts
@@ -1,12 +1,17 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map, tap } from "rxjs";
+import { map, tap, take, exhaustMap } from "rxjs";
+import { AuthService } from "../auth/auth.service";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
@@ -19,20 +24,28 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.http
-      .get<Recipe[]>('https://ng-course-recipe-book-e8fd7-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
-      .pipe(
-        map(recipes => {
-          return recipes.map(recipe => {
-            return {
-              ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : []
-            };
-          })
-        }),
-        tap(recipes => {
-          this.recipeService.setRecipes(recipes)
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap(user => {
+        return this.http
+        .get<Recipe[]>(
+          'https://ng-course-recipe-book-e8fd7-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+          {
+            params: new HttpParams().set('auth', user.token)
+          }
+        )
+      }),
+      map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : []
+          };
         })
-      );
+      }),
+      tap(recipes => {
+        this.recipeService.setRecipes(recipes)
+      })
+    );
   }
 }
