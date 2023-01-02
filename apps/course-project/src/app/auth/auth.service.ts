3daa10187870d6c3c13e24702f936c246e2aa3c1
Timo Nohe <timo.ademeit@gmx.de>
2023-01-02 12:06:33 +0100

    298. Login Error Handling

--- apps/course-project/src/app/auth/auth.service.ts
+++ apps/course-project/src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
@@ -21,27 +21,35 @@ export class AuthService {
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCg_8wO11MXzKzaY2V09hro-T4YBizomxU',
       { email, password, returnSecureToken: true }
     )
-    .pipe(
-      catchError(errorRes => {
-        let errorMessage = 'An unknown error occured!';
-        if (!errorRes.error || !errorRes.error.error) {
-          return throwError(() => new Error(errorMessage));
-        }
-
-        switch(errorRes.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = 'This email already exists!';
-        }
-
-        return throwError(() => new Error(errorMessage));
-      })
-    );
+    .pipe(catchError(this.handleError));
   }
 
   login(email: string, password: string) {
     return this.http.post<AuthResponseData>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCg_8wO11MXzKzaY2V09hro-T4YBizomxU',
       { email, password, returnSecureToken: true }
-    );
+    )
+    .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occured!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(() => new Error(errorMessage));
+    }
+
+    switch(errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email already exists!';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct.';
+        break;
+    }
+
+    return throwError(() => new Error(errorMessage));
   }
 }
