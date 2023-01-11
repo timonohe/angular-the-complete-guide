7fac88aa99e55aba42cd25ca8053a6c219ec9d31
Timo Nohe <timo.ademeit@gmx.de>
2023-01-11 11:48:10 +0100

    312. Using ngIf

--- apps/course-project/src/app/shared/alert/alert.component.ts
+++ apps/course-project/src/app/shared/alert/alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
   selector: 'course-project-alert',
@@ -7,4 +7,9 @@ import { Component, Input } from "@angular/core";
 })
 export class AlertComponent {
   @Input() message: string;
+  @Output() closeAlert = new EventEmitter<void>();
+
+  onClose() {
+    this.closeAlert.emit();
+  }
 }
