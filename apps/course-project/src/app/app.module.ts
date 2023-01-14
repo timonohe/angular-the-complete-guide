ed84ba02cc2b3244f1efe9a3a1eb967f94b88c79
Timo Nohe <timo.ademeit@gmx.de>
2023-01-14 13:00:22 +0100

    331. Implementing Lazy Loading

--- apps/course-project/src/app/app.module.ts
+++ apps/course-project/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core.module';
 import { HeaderComponent } from './header/header.component';
-import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
@@ -20,7 +19,6 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule,
     ShoppingListModule,
     AuthModule,
     SharedModule,
