49b3457aa1489a3b5a38acfc321e69e6ccf25ae0
Timo Nohe <timo.ademeit@gmx.de>
2023-02-22 12:19:58 +0100

    357. Getting Started with Reducers

---
+++ apps/course-project/src/app/shopping-list/shopping-list.reducer.ts
@@ -0,0 +1,10 @@
+const initialState = {
+  ingredients: [
+    { name: 'Apples', amount: 5 },
+    { name: 'Tomatoes', amount: 10 },
+  ]
+};
+
+export function shoppingListReducer(state = initialState, action) {
+  
+}
